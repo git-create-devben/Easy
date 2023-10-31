d4aa364b22d071e0ad138dfa0cd4d317f949bc11
git-create-devben <DevBen.tech@outlook.com>
2023-10-31 17:09:27 +0100

    add desc

--- Easy/src/pages/index.js
+++ Easy/src/pages/index.js
@@ -12,8 +12,8 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">Easy</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className="hero__title">Easy Sign Up Pages Component</h1>
+        <p className="hero__subtitle">Easy Sign Up Pages Component is a free and open-source component library that makes it easy to create sign up pages with Tailwind CSS, Vanilla CSS, and Bootstrap. It works with Next.js, HTML, and React, and more components like footers and navbars will be added in the future.</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
